3a7a6bb7e1ab92c64b424c1607cfee1bfa79c4d9
Thomas Harvey PhD2022 <tharvey@morgan.jb.man.ac.uk>
2024-08-19 16:42:36 +0100

    come on

--- ResolvedGalaxy.py
+++ ResolvedGalaxy.py
@@ -4303,10 +4303,7 @@ if __name__ == "__main__":
     sfh["age"] = (0.001, 15) # Gyr
     sfh['age_prior'] = 'uniform'
     sfh['metallicity_prior'] = 'uniform'
-    if metallicity_prior == 'log_10':
-        sfh["metallicity"] = (1e-03, 3)
-    elif metallicity_prior == 'uniform':
-        sfh['metallicity'] = (0, 3)
+    sfh['metallicity'] = (0, 3)
 
     
     dust = {}
