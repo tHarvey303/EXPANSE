643e781487ae083b8ca53c017d87038e6ca45532
Thomas Harvey <tharvey303@gmail.com>
2024-11-01 12:27:59 +0000

    come on

--- src/EXPANSE/bagpipes/__init__.py
+++ src/EXPANSE/bagpipes/__init__.py
@@ -1,9 +1,11 @@
 from .pipes_models import (
     delayed_dict,
     continuity_dict,
+    continuity_bursty_dict,
     dpl_dict,
     lognorm_dict,
-    resolved_dict,
+    resolved_dict_cnst,
+    resolved_dict_bursty,
     create_dicts,
 )
 
@@ -16,4 +18,4 @@ from .plotpipes import (calculate_bins,
 
 
 __all__ = ['calculate_bins', 'combine_bands', 'five_sig_depth_to_n_sig_depth', 'colormap', 'PipesFit', 'PlotPipes', 
-              'delayed_dict', 'continuity_dict', 'dpl_dict', 'lognorm_dict', 'resolved_dict', 'create_dicts']
+              'delayed_dict', 'continuity_dict', 'dpl_dict', 'lognorm_dict', 'resolved_dict_cnst', 'create_dicts', 'resolved_dict_bursty', 'continuity_bursty_dict']
