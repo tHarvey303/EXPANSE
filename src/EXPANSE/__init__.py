1996fd268ae56e726a644523ea064dc49cbaf2b3
tHarvey303 <tHArvey303@gmail.com>
2025-06-01 01:32:02 +0100

    Added utlity functions for creating correlated noise to match an observation, and diagnostics for plotting auto-correlation of pixel values in imaging.

--- src/EXPANSE/__init__.py
+++ src/EXPANSE/__init__.py
@@ -3,9 +3,10 @@ from .ResolvedGalaxy import (ResolvedGalaxy,
                             ResolvedGalaxies,
                             run_bagpipes_wrapper)
 
-from .utils import (make_EAZY_SED_fit_params_arr, update_mpl, scale_fluxes, CLIInterface, is_cli, PhotometryBandInfo, FieldInfo, compass, measure_cog, optimize_sfh_xlimit,
-    create_fitsmap, PhotometryBandInfo, FieldInfo, display_fitsmap, renorm_psf, suppress_stdout_stderr,
-    plot_with_shadow, gradient_path_effect)
+from .utils import (make_EAZY_SED_fit_params_arr, update_mpl, scale_fluxes, 
+                    CLIInterface, is_cli, PhotometryBandInfo, FieldInfo, compass, measure_cog, optimize_sfh_xlimit,
+                    create_fitsmap, display_fitsmap, renorm_psf, suppress_stdout_stderr,
+                    plot_with_shadow, gradient_path_effect, model_and_apply_correlated_noise)
 
 
 from .bagpipes.plotpipes import (calculate_bins, PlotPipes, PipesFit)
@@ -25,8 +26,14 @@ from . import vis
 from .gui.ResolvedSEDApp import expanse_viewer
 
 __all__ = ['ResolvedGalaxy', 'MockResolvedGalaxy', 'ResolvedGalaxies',
-           'convert_coordinates', 'apply_pixel_coordinate_mask', 'get_spectra_in_mask', 
-           'calculate_sfh', 'make_EAZY_SED_fit_params_arr', 'update_mpl', 
+           'make_EAZY_SED_fit_params_arr', 'update_mpl', 
            'scale_fluxes', 'CLIInterface', 'is_cli', 'calculate_bins', 
-           'PlotPipes', 'PipesFit', 'bagpipes', 'expanse_viewer', 'expanse_viewer']
+           'PlotPipes', 'PipesFit', 'bagpipes', 'expanse_viewer', 'expanse_viewer',
+           'make_EAZY_SED_fit_params_arr', 'FieldInfo', 'PhotometryBandInfo',
+           'update_mpl', 'compass', 'measure_cog', 'optimize_sfh_xlimit',
+           'create_fitsmap', 'display_fitsmap', 'renorm_psf',
+           'suppress_stdout_stderr', 'plot_with_shadow',
+           'gradient_path_effect', 'model_and_apply_correlated_noise']
+
+
 
